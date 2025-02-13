237a73c41d501fab7ebfb9d26a3ec9a75e5358a6
Kip Hart <41959581+kip-hart@users.noreply.github.com>
2025-02-12 23:36:40 -0800

    try legit script

--- .github/scripts/run_all_scripts.m
+++ .github/scripts/run_all_scripts.m
@@ -1,5 +1,24 @@
 function run_all_scripts(directory)
-    disp('Hello World!')
+    % Get the list of all files and folders in the directory
+    files = dir(directory);
+
+    % Loop through each item in the directory
+    for i = 1:length(files)
+        % Skip '.' and '..' directories
+        if strcmp(files(i).name, '.') || strcmp(files(i).name, '..')
+            continue;
+        end
+        
+        % Get the full path of the item
+        fullPath = fullfile(directory, files(i).name);
+        [~, ~, ext] = fileparts(fullPath);
+        % If the item is a folder, recursively call the function
+        if files(i).isdir
+            run_all_scripts(fullPath);
+        elseif strcmp(ext, '.m') && ~isFunction(fullPath)
+            run_single_script(fullPath);
+        end
+    end
 end
 
 %{
@@ -35,7 +54,7 @@ function run_all_scripts(directory)
     end
     %}
 end
-
+%}
 function flag = isFunction(fullPath)
     % Read the first line of the file
     fid = fopen(fullPath, 'r');
@@ -48,7 +67,7 @@ function flag = isFunction(fullPath)
     firstLine = fgetl(fid);
     fclose(fid);
 
-    flag = contains(strtrim(firstLine), 'function');
+    flag = startsWith(strtrim(firstLine), 'function');
 end
 
 function run_single_script(fullPath)
@@ -72,4 +91,3 @@ function dyPath = diary_filename(fullPath)
     [path, name, ~] = fileparts(fullPath);
     dyPath = fullfile(path, [name '.diary']);
 end
-%}
