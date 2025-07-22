f2084cd91974db400101886d16fd00cc07e1a3ce
Kip Hart <kiphart91@gmail.com>
2025-07-22 07:31:15 -0700

    typo

--- matlab/poly_pumpkin_cannon_polyder.m
+++ matlab/poly_pumpkin_cannon_polyder.m
@@ -9,4 +9,4 @@ r_speed = roots(p_speed)
 %% Find max height
 h_max = polyval(p, r_speed)
 disp(['Max height of ' num2str(h_max/U.FT) ...
-      ' achieved at ' numstr(r_speed/U.S) ' s'])
+      ' achieved at ' num2str(r_speed/U.S) ' s'])
